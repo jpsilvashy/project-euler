3d64986063b3df974d4cb8146ca45a953bd479c4
Joseph Silvashy <jpsilvashy@gmail.com>
2012-03-19 19:00:45 -0700

    #2 working! gets correct answer in (0.174047) < 2ms, could be better

--- ruby/problem_003.rb
+++ ruby/problem_003.rb
@@ -9,30 +9,45 @@ require "benchmark"
 
 val = ARGV[0].to_i
 
-def is_prime?(n)
-  for d in 2..(Math.sqrt(n))
-   if (n % d) == 0
-    return false
+# add some methods to Integer
+class Integer
+
+  # return array of factors
+  def factors
+    1.upto(Math.sqrt(self)).select {|i| (self % i).zero?}.inject([]) do |factors, i|
+      factors << i
+      factors << self/i unless i == self/i
+      factors
+    end.sort
   end
+
+  # is the integer a prime number?
+  def is_prime?
+    for d in 2..(Math.sqrt(self))
+      if (self % d) == 0
+        return false
+      end
+    end
+    true
+  end
+
 end
 
-true
+def prime_factors_for(int)
+  # get the factors for the int provided
+  factors = int.factors
+
+  # check if the factors are prime, otherwise pop them
+  until factors.last.is_prime?
+    factors.pop
+  end
+
+  # return the last item in the array because it's prime
+  factors.last
 end
 
 time = Benchmark.measure do
-  count = val
-  val.times do
-    if count.even?
-      count -= 1
-    else
-      if is_prime?(count) == true
-        puts "\n#{count} is prime!"
-        break
-      end
-      print '.'
-      count -= 2
-    end
-  end
+  puts prime_factors_for(val)
 end
 
 puts time
