fa22c38f5c490bed9128f88b797620c8837b06a5
jpsilvashy <jpsilvashy@gmail.com>
2012-03-29 12:40:23 -0700

    got number 9 ( 0.728874)

--- ruby/problem_009.rb
+++ ruby/problem_009.rb
@@ -13,6 +13,17 @@
 
 require "benchmark"
 
+time = Benchmark.measure do
+
+  (1..1000).each do |a|
+    (1..1000).each do |b|
+      c = Math.sqrt(a**2 + b**2)
+      if (a + b + c == 1000) && (a < b)
+        puts (a * b * c).to_i
+      end
+    end
+  end
+
 end
 
 puts time
